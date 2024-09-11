2b42dc3b6d48645cce0084d05d5e0330efef9c15
DmitrySimonov <pokefrost@gmail.com>
2024-09-11 11:54:23 +0200

    fix config

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
    darkMode: ['class'],
-   content: ['./src/**/*.{html, js, tsx, ts}'],
+   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
    theme: {
       extend: {
          borderRadius: {
