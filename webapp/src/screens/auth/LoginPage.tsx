fb56cf5f66dd22ebff95985a92cd1878e794bdf3
tyi-dev <trykoz.yaroslav.ty@gmail.com>
2024-09-11 11:40:01 +0200

    to fix tailwind

--- webapp/src/screens/auth/LoginPage.tsx
+++ webapp/src/screens/auth/LoginPage.tsx
@@ -2,9 +2,9 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import LoginSchema from '@shared/src/schemas/login';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { Button } from '@webapp/components/ui/button';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@webapp/components/ui/form';
-import { Input } from '@webapp/components/ui/input';
+import { Button } from '@webapp/src/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@webapp/src/components/ui/form';
+import { Input } from '@webapp/src/components/ui/input';
 
 export default function LoginPage() {
    const form = useForm<z.infer<typeof LoginSchema>>({
@@ -20,36 +20,39 @@ export default function LoginPage() {
    }
 
    return (
-      <Form {...form}>
-         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-               control={form.control}
-               name="email"
-               render={({ field }) => (
-                  <FormItem>
-                     <FormLabel>Email</FormLabel>
-                     <FormControl>
-                        <Input placeholder="Email" {...field} />
-                     </FormControl>
-                     <FormMessage />
-                  </FormItem>
-               )}
-            />
-            <FormField
-               control={form.control}
-               name="password"
-               render={({ field }) => (
-                  <FormItem>
-                     <FormLabel>Password</FormLabel>
-                     <FormControl>
-                        <Input placeholder="Email" {...field} />
-                     </FormControl>
-                     <FormMessage />
-                  </FormItem>
-               )}
-            />
-            <Button type="submit">Submit</Button>
-         </form>
-      </Form>
+      <>
+         <p className="text-4xl">ASDAD</p>
+         <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+               <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                     <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                           <Input placeholder="Email" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                     </FormItem>
+                  )}
+               />
+               <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                     <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                           <Input placeholder="Email" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                     </FormItem>
+                  )}
+               />
+               <Button type="submit">Submit</Button>
+            </form>
+         </Form>
+      </>
    );
 }
