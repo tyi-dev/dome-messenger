8ff09e640667524aaa4d99cef65f63e7b55414f8
tyi-dev <trykoz.yaroslav.ty@gmail.com>
2024-10-17 15:30:20 +0200

    add lastSeen util func

---
+++ shared/src/utils/lastSeen.ts
@@ -0,0 +1,8 @@
+import { User } from '@shared/types/user';
+import { formatDistance } from 'date-fns';
+
+const returnLastSeen = (user: User) => {
+   if (user.lastSeen) return `last seen ${formatDistance(user.lastSeen, new Date())} ago`;
+};
+
+export default returnLastSeen;
